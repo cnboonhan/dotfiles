d48c9eb07369c8df4a0cc8e8c61524d2ff13b8a6
Charayaphan Nakorn Boon Han <charayaphan.nakorn.boon.han@gmail.com>
2021-08-10 11:32:55 +0800

    add bat
    Signed-off-by: Charayaphan Nakorn Boon Han <charayaphan.nakorn.boon.han@gmail.com>

--- scripts/dependencies.bash
+++ scripts/dependencies.bash
@@ -11,13 +11,18 @@ set -o nounset
 set -o pipefail
 
 export PROJECT=dependencies-setup
-. ./utils.bash
+SCRIPTPATH="$(
+    cd -- "$(dirname "$0")" >/dev/null 2>&1
+    pwd -P
+)"
+. "$SCRIPTPATH/utils.bash"
 
 [ "$EUID" -ne 0 ] && echo "Please run as root." && exit 1
 
-: "${HEADLESS:=1}"
+: "${GUI:=1}"
+: "${MISC:=1}"
 
-apt update 
+apt update
 
 __msg_info "Installing general apts"
 DEBIAN_FRONTEND=noninteractive apt install \
@@ -30,14 +35,20 @@ DEBIAN_FRONTEND=noninteractive apt install \
     shellcheck \
     xclip \
     nload tshark termshark nmap \
-    -y  || __error_exit $LINENO "Something wrong happened with installing apt dependencies."
+    -y || __error_exit $LINENO "Something wrong happened with installing apt dependencies."
 
 __msg_info "Installing GUI related apts"
-( [ "$HEADLESS" == "0" ] && DEBIAN_FRONTEND=noninteractive apt install ubuntu-desktop gnome-panel gnome-settings-daemon xfce4 nautilus gnome-terminal ) \
-    || __msg_info "Did not install GUI related dependencies. set HEADLESS=0 to install GUI dependencies."
+([ "$GUI" == "1" ] && DEBIAN_FRONTEND=noninteractive apt install ubuntu-desktop gnome-panel gnome-settings-daemon xfce4 nautilus gnome-terminal) ||
+    __msg_info "Did not install GUI related dependencies. set GUI=1 to install GUI dependencies."
 
 __msg_info "Installing snaps"
-command -v || __error_exit $LINENO "No snaps avaiable."
+command -v snap >/dev/null || __error_exit $LINENO "No snaps avaiable."
 snap install lxd
 snap install --beta prettier
 snap install shfmt
+
+__msg_info "Installing misc"
+(
+    [ "$MISC" == "1" ] && \
+        DEBIAN_FRONTEND=noninteractive apt install -o Dpkg::Options::="--force-overwrite" bat -y && (ln -s /usr/bin/batcat /usr/bin/bat 2> /dev/null || true)
+) || __msg_info "Did not install misc related dependencies. Set MISC=1 to install MISC dependencies."
