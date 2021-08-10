5fb449059061e175725d2b4df9f313ff2e59a228
Charayaphan Nakorn Boon Han <charayaphan.nakorn.boon.han@gmail.com>
2021-08-10 10:41:17 +0800

    add updates

--- scripts/dependencies.bash
+++ scripts/dependencies.bash
@@ -15,8 +15,11 @@ export PROJECT=dependencies-setup
 
 [ "$EUID" -ne 0 ] && echo "Please run as root." && exit 1
 
+: "${HEADLESS:=1}"
+
 apt update 
 
+__msg_info "Installing general apts"
 DEBIAN_FRONTEND=noninteractive apt install \
     git curl wget openssh-server \
     tmux vim ripgrep fzf \
@@ -24,16 +27,17 @@ DEBIAN_FRONTEND=noninteractive apt install \
     flake8 yamllint jq python3-autopep8 clang-format \
     build-essential clang bear gdb cmake exuberant-ctags \
     lightdm dwm stterm \
+    shellcheck \
     xclip \
     nload tshark termshark nmap \
     -y  || __error_exit $LINENO "Something wrong happened with installing apt dependencies."
 
-DEBIAN_FRONTEND=noninteractive apt install \
-    ubuntu-desktop gnome-panel gnome-settings-daemon xfce4 nautilus gnome-terminal || __error_exit $LINENO "Something happened with installing apt dependencies for GUI"
-
+__msg_info "Installing GUI related apts"
+( [ "$HEADLESS" == "0" ] && DEBIAN_FRONTEND=noninteractive apt install ubuntu-desktop gnome-panel gnome-settings-daemon xfce4 nautilus gnome-terminal ) \
+    || __msg_info "Did not install GUI related dependencies. set HEADLESS=0 to install GUI dependencies."
 
 __msg_info "Installing snaps"
-command -v || { echo -e "No snap installs available" && exit 1; }
+command -v || __error_exit $LINENO "No snaps avaiable."
 snap install lxd
 snap install --beta prettier
 snap install shfmt
